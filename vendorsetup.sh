97839115d6f8d2080def0c196390b69b2ec3b1a4
CTXz <ctx.xda@gmail.com>
2016-10-28 00:07:40 +0200

    Mount ota-tools only once!
    Change-Id: I24dd07222f9bf5c4d25e9e467dd38ea010be3263

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,7 @@
-sudo mount -t tmpfs -o size=8589934592,mode=1777 ota-tools /tmp
+if [[ ! $(df -h | grep ota-tools) ]];
+then
+	sudo mount -t tmpfs -o size=8589934592,mode=1777 ota-tools /tmp
+fi
+
 source ~/venv/bin/activate
 LANG=C
